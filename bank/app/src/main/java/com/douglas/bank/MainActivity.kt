f3615d4eb704d25ddfc952bb1037c59c3b84e14b
Douglas Carvalho <douglaspdc@gmail.com>
2019-04-23 22:47:08 -0300

    Realiza migração para o AndroidX

--- bank/app/src/main/java/com/douglas/bank/MainActivity.kt
+++ bank/app/src/main/java/com/douglas/bank/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.douglas.bank
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.douglas.actions.Actions
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         startActivity(Actions.getLoginIntent(this))
     }
--- bank/features/login/src/main/java/com/douglas/login/LoginActivity.kt
+++ bank/features/login/src/main/java/com/douglas/login/LoginActivity.kt
@@ -1,7 +1,7 @@
 package com.douglas.login
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
 
