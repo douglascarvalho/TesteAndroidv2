367c07905ddea7a8e15ee8bcddf3bb42d85c0484
Douglas Carvalho <douglas.de.carvalho@accenture.com>
2019-04-29 09:13:18 -0300

    Add initial StatementActivity

---
+++ bank/features/statement/src/main/java/com/doug/statement/StatementActivity.kt
@@ -0,0 +1,14 @@
+package com.doug.statement
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StatementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statement)
+
+    }
+
+}
--- bank/libraries/actions/src/main/java/com/douglas/actions/Actions.kt
+++ bank/libraries/actions/src/main/java/com/douglas/actions/Actions.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 object Actions {
 
     fun getLoginIntent(context: Context): Intent = internalIntent(context, "com.douglas.intent.action.LOGIN")
+    fun getStatementIntent(context: Context): Intent = internalIntent(context, "com.douglas.intent.action.STATEMENT")
 
     private fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
 }
